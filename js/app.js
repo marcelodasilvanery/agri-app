615ec4a8f1adc618caf2919aef485e6610cf6cf6
marcelodasilvanery <marcelo.nery.estiva@gmail.com>
2026-01-30 14:11:24 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 // js/app.js
-let map;
+export let map;
 
 export function initMap() {
-  if (map) return; // evita reinicializar
+  if (map) return;
 
   map = L.map("map", {
-    center: [-15.7801, -47.9292], // centro Brasil
+    center: [-15.7801, -47.9292],
     zoom: 5,
-    zoomControl: true,
   });
 
-  // OpenStreetMap
   const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "© OpenStreetMap",
   });
 
-  // ESRI Satélite
   const esriSat = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     { maxZoom: 19, attribution: "© Esri" }
