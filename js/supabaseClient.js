8086f4325215a119eee88e98ab8434ae43ff845c
marcelodasilvanery <marcelo.nery.estiva@gmail.com>
2026-01-30 14:08:51 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,8 @@
 // js/supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+// Substitua pelo seu Project URL e Publishable Key
 const SUPABASE_URL = "https://ucqxfreyypdwkiibgdvb.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_PUBLISHABLE_KEY_AQUI";
+const SUPABASE_ANON_KEY = "sb_publishable_9hETD38TstyiNqwpa63Myg_0ZKCH0wF";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
