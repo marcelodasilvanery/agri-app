016b2cf133087503f77c58962912372634501c30
marcelodasilvanery <marcelo.nery.estiva@gmail.com>
2026-01-30 12:03:35 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-console.log("App JS carregado com sucesso");
+// PROVA DE VIDA
+console.log("app.js carregado corretamente");
+alert("JS externo executando");
 
 // Inicializa o mapa
 var map = L.map('map').setView([-15.78, -47.93], 6);
 
-// OpenStreetMap
-var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-});
-
-// ESRI Satélite
-var esri = L.tileLayer(
-    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    {
-        attribution: 'Tiles © Esri'
-    }
+// Camadas base
+var osm = L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    { attribution: '© OpenStreetMap' }
 );
 
-// Camada padrão
+var esri = L.tileLayer(
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    { attribution: 'Tiles © Esri' }
+);
+
+// Adiciona camada padrão
 osm.addTo(map);
 
 // Controle de camadas
