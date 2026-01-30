e70fb327e65ce180ee659eac730e44aa764ca4f5
marcelodasilvanery <marcelo.nery.estiva@gmail.com>
2026-01-30 13:41:16 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ucqxfreyypdwkiibgdvb.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_9hETD38TstyiNqwpa63Myg_0ZKCH0wF
+";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
